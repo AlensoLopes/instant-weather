7df3bd4bd47abdd9e03374431fea0b55710dbec6
Julien Sailly <juliensailly76@gmail.com>
2023-09-20 23:57:07 +0200

    feat: minor styles changes + search behaviour improved

--- script/app.js
+++ script/app.js
@@ -1,11 +1,24 @@
-let searchInput = document.getElementById("search"), WeatherApiToken = "6051ed4396ddb33dd0a53913aa5479b93328e2784fab430693ed7cbe340d9557";
+let searchInput = document.getElementById("search"),
+    searchButton = document.getElementById("search-btn"),
+    WeatherApiToken =
+        "6051ed4396ddb33dd0a53913aa5479b93328e2784fab430693ed7cbe340d9557";
 
 /*
-* Search for a cities in the API when the user types in the search bar
-*/
-function search() {
+ * Search for a cities in the API when the user types in the search bar
+ */
+function searchInputChanged() {
     let cityName = searchInput.value;
-    let apiURL = "https://geo.api.gouv.fr/communes?nom="+cityName+"&fields=nom,code,codesPostaux&boost=population&limit=5"
+    let apiURL =
+        "https://geo.api.gouv.fr/communes?nom=" +
+        cityName +
+        "&fields=nom,code,codesPostaux&boost=population&limit=5";
+    // Check if the value is a postal code
+    if (!isNaN(cityName)) {
+        apiURL =
+            "https://geo.api.gouv.fr/communes?codePostal=" +
+            cityName +
+            "&fields=nom,code,codesPostaux";
+    }
 
     const xhttpr = new XMLHttpRequest();
     xhttpr.open("GET", apiURL, true);
@@ -17,7 +30,11 @@ function search() {
         if (xhttpr.status === 200) {
             const response = JSON.parse(xhttpr.response);
             // console.log(response);
-            displayCitiesGuesses(response);
+            if (response.length == 0 && cityName.length > 0) {
+                displayCitiesGuesses(response, true);
+            } else {
+                displayCitiesGuesses(response);
+            }
         } else {
             alert("The request failed!");
         }
@@ -25,9 +42,47 @@ function search() {
 }
 
 /*
-* Display the cities guesses in the HTML page
-*/
-function displayCitiesGuesses(citiesGuesses) {
+ * Search for the specific city in the API when the user clicks on the search button
+ */
+function searchSubmit() {
+    // let cityName = searchInput.value;
+    // let apiURL =
+    //     "https://geo.api.gouv.fr/communes?nom=" +
+    //     cityName +
+    //     "&fields=nom,code,codesPostaux&boost=population&limit=5";
+    // // Check if the value is a postal code
+    // if (!isNaN(cityName)) {
+    //     apiURL =
+    //         "https://geo.api.gouv.fr/communes?codePostal=" +
+    //         cityName +
+    //         "&fields=nom,code,codesPostaux";
+    // }
+
+    // const xhttpr = new XMLHttpRequest();
+    // xhttpr.open("GET", apiURL, true);
+
+    // xhttpr.send();
+    // // console.log("Request sent : "+apiURL);
+
+    // xhttpr.onload = () => {
+    //     if (xhttpr.status === 200) {
+    //         const response = JSON.parse(xhttpr.response);
+    //         // console.log(response);
+    //         if (response.length == 1) {
+    //             cityChoiceMade(response[0]);
+    //         } else {
+    //             displayCitiesGuesses(response);
+    //         }
+    //     } else {
+    //         alert("The request failed!");
+    //     }
+    // };
+}
+
+/*
+ * Display the cities guesses in the HTML page
+ */
+function displayCitiesGuesses(citiesGuesses, isNoCitiesFound) {
     let cityGuessesDiv = document.getElementById("cityGuesses");
 
     let cityGuesses = document.getElementsByClassName("cityGuess");
@@ -35,37 +90,50 @@ function displayCitiesGuesses(citiesGuesses) {
         cityGuesses[0].parentNode.removeChild(cityGuesses[0]);
     }
 
-    for (let i = 0; i < citiesGuesses.length; i++) {
-        let city = citiesGuesses[i];
+    if (isNoCitiesFound) {
         let cityDiv = document.createElement("div");
         cityDiv.classList.add("cityGuess");
-        cityDiv.innerHTML = city.nom;
-        cityDiv.addEventListener("click", () => cityChoiceMade(city));
+        cityDiv.innerHTML = "No cities found";
         cityGuessesDiv.appendChild(cityDiv);
+    } else {
+        for (let i = 0; i < citiesGuesses.length; i++) {
+            let city = citiesGuesses[i];
+            let cityDiv = document.createElement("div");
+            cityDiv.classList.add("cityGuess");
+            cityDiv.innerHTML = city.nom;
+            cityDiv.addEventListener("click", () => cityChoiceMade(city));
+            cityGuessesDiv.appendChild(cityDiv);
+        }
     }
 }
 
 /*
-* Retrieve the weather data from the API
-*/
+ * Retrieve the weather data from the API
+ */
 function cityChoiceMade(city) {
-    console.log("City choice made : "+city.nom+" ("+city.code+")");
-    let cityName = city.nom, codeINSEE = city.code, postalCodes = city.codesPostaux;
+    searchInput.value = city.nom;
+    let cityGuesses = document.getElementsByClassName("cityGuess");
+    while (cityGuesses[0]) {
+        cityGuesses[0].parentNode.removeChild(cityGuesses[0]);
+    }
 
-    document.getElementById("city-name").textContent = cityName + " ("+postalCodes[0]+")";
+    console.log("City choice made : " + city.nom + " (" + city.code + ")");
+    let cityName = city.nom,
+        codeINSEE = city.code,
+        postalCodes = city.codesPostaux;
 
-    
+    document.getElementById("city-name").textContent =
+        cityName + " (" + postalCodes[0] + ")";
 }
 
 /*
-* Display meteo infos in the HTML page
-*/
-function displayMeteoInfos(meteoInfos) {
-
-}
+ * Display meteo infos in the HTML page
+ */
+function displayMeteoInfos(meteoInfos) {}
 
 function onPageLoad() {
-    searchInput.addEventListener("input", () => search());
+    searchInput.addEventListener("input", () => searchInputChanged());
+    searchButton.addEventListener("click", () => searchSubmit());
 }
 
 onPageLoad();
