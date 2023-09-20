205e432cfd9ddb97da23770327487049be3b7cf2
Julien Sailly <juliensailly76@gmail.com>
2023-09-20 23:21:56 +0200

    refactor: minor changes

--- script/app.js
+++ script/app.js
@@ -1,26 +1,32 @@
-let searchInput = document.getElementById("search");
+let searchInput = document.getElementById("search"), WeatherApiToken = "6051ed4396ddb33dd0a53913aa5479b93328e2784fab430693ed7cbe340d9557";
 
+/*
+* Search for a cities in the API when the user types in the search bar
+*/
 function search() {
     let cityName = searchInput.value;
-    let apiURL = "https://geo.api.gouv.fr/communes?nom="+cityName+"&fields=departement&boost=population&limit=5"
+    let apiURL = "https://geo.api.gouv.fr/communes?nom="+cityName+"&fields=nom,code,codesPostaux&boost=population&limit=5"
 
     const xhttpr = new XMLHttpRequest();
     xhttpr.open("GET", apiURL, true);
 
     xhttpr.send();
-    console.log("Request sent : "+apiURL);
+    // console.log("Request sent : "+apiURL);
 
     xhttpr.onload = () => {
         if (xhttpr.status === 200) {
             const response = JSON.parse(xhttpr.response);
-            console.log(response);
+            // console.log(response);
             displayCitiesGuesses(response);
         } else {
-            console.log("The request failed!");
+            alert("The request failed!");
         }
     };
 }
 
+/*
+* Display the cities guesses in the HTML page
+*/
 function displayCitiesGuesses(citiesGuesses) {
     let cityGuessesDiv = document.getElementById("cityGuesses");
 
@@ -34,8 +40,28 @@ function displayCitiesGuesses(citiesGuesses) {
         let cityDiv = document.createElement("div");
         cityDiv.classList.add("cityGuess");
         cityDiv.innerHTML = city.nom;
+        cityDiv.addEventListener("click", () => cityChoiceMade(city));
         cityGuessesDiv.appendChild(cityDiv);
     }
+}
+
+/*
+* Retrieve the weather data from the API
+*/
+function cityChoiceMade(city) {
+    console.log("City choice made : "+city.nom+" ("+city.code+")");
+    let cityName = city.nom, codeINSEE = city.code, postalCodes = city.codesPostaux;
+
+    document.getElementById("city-name").textContent = cityName + " ("+postalCodes[0]+")";
+
+    
+}
+
+/*
+* Display meteo infos in the HTML page
+*/
+function displayMeteoInfos(meteoInfos) {
+
 }
 
 function onPageLoad() {
